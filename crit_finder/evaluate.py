3bb4aec8360a2456dd107cb6cb0b6653091233e3
Charles Frye <charlesfrye@berkeley.edu>
2018-02-20 19:58:18 -0800

    adds doc strings to evaluate

--- crit_finder/evaluate.py
+++ crit_finder/evaluate.py
@@ -1,3 +1,4 @@
+"""Helper functions for running various tests and plotting their results."""
 from . import graphs
 
 from matplotlib import pyplot as plt
@@ -8,13 +9,24 @@ import numpy as np
 import scipy.integrate
 
 
-# functions for testing the performance of descent methods on quadratic forms
+## functions for testing the performance of descent methods on quadratic forms
 
 def compare_descent_methods(matrix_generator, N=5, num_steps=5, num_matrices=10, num_runs=5,
                             methods=["gradient_descent","newton"],
                            hyperparameters={"learning_rate":0.1, "newton_rate":1,
                                             "fudge_factor":1e-6, "inverse_method":"fudged"}):
-    
+    """compare the gradient norms found by descent methods in methods list
+    on num_matrices quadratic forms whose matrices are produced by matrix generator,
+    which should be a function that takes a single argument, N, and returns a N by N matrix.
+
+    The algorithms are both run from num_runs different initializations,
+    and, for ease of implementation, num_steps *times*, with increasing numbers of steps.
+    This is woefully inefficient, but works alright for num_steps less than 3.
+
+    The results are returned as a 4-D array, results, with dimensions
+    [num_steps+1, num_matrices, num_runs, len(methods)].
+    """
+
     num_methods = len(methods)
     matrices = [matrix_generator(N) for _ in range(num_matrices)]
 
@@ -29,13 +41,24 @@ def compare_descent_methods(matrix_generator, N=5, num_steps=5, num_matrices=10,
             quadratic_form = graphs.make_quadratic_form(matrix, initial_values, hyperparameters)
             for step_idx in range(num_steps+1):
                 for method_idx, method in enumerate(methods):
-                    _, values =  graphs.minimize(quadratic_form, method, step_idx)        
-                    results[step_idx, matrix_idx, run_idx, method_idx] = graphs.get_result("gradient_norm", 
+                    _, values =  graphs.minimize(quadratic_form, method, step_idx)
+                    results[step_idx, matrix_idx, run_idx, method_idx] = graphs.get_result("gradient_norm",
                                                                            values, quadratic_form)
 
     return results
 
+def normalize_runs(results):
+    """normalize results from compare_gradient_descent_methods so that each run
+    starts with gradient_norm standardized to 1.
+    """
+    results = np.divide(results, results[0,:,:,:][None,:,:,:])
+    return results
+
 def gradient_test(N, matrix_generator, algorithm, num_steps, hyperparameters=graphs.DEFAULTS):
+    """output the initial and final outputs and gradient norms found by taking num_steps of algorithm
+    with provided hyperparameters on a matrix returned by matrix_generator,
+    a function that takes an argument N and returns an N by N matrix.
+    """
 
     random_matrix = matrix_generator(N)
 
@@ -57,14 +80,14 @@ def gradient_test(N, matrix_generator, algorithm, num_steps, hyperparameters=gra
           "\tinitial: {0}".format(np.linalg.norm(initial_gradients, 2)),
           "\tfinal: {0}".format(np.linalg.norm(final_gradients, 2)))
 
-def normalize_runs(results):
-    results = np.divide(results, results[0,:,:,:][None,:,:,:])
-    return results
 
 # functions for plotting performance on quadratic forms
 
 def plot_trajectories_comparison(results, methods=["gradient_descent","newton"],
                                  colors=["salmon","darkolivegreen","medium_blue"]):
+    """plots the (normalized) gradient norms in results as a function of step count,
+    for each method in methods, with colors given by colors.
+    """
     f = plt.figure(figsize=(16,4))
     ax = plt.subplot(111)
     for matrix_idx in range(results.shape[1]):
@@ -72,7 +95,7 @@ def plot_trajectories_comparison(results, methods=["gradient_descent","newton"],
             color = colors[method_idx]
             method = methods[method_idx]
             plt.plot(results[:,matrix_idx,:,method_idx], color=color, linewidth=1, alpha=0.5, label=method);
-        
+
     plt.legend()
     ax.set_ylabel(r"Normalized $\|\nabla f\|^2$",fontsize=16, fontweight="bold")
     ax.set_xlabel("Iteration Number",fontsize=16, fontweight="bold")
@@ -81,17 +104,20 @@ def plot_trajectories_comparison(results, methods=["gradient_descent","newton"],
     ax.set_xticks(range(0,1+int(max(ax.get_xlim()))));
 
 def plot_benefit(results, methods=["gradient_descent","newton"]):
+    """plots the difference in gradient norm between methods[1] and methods[0]
+    on a per-run basis.
+    """
     f = plt.figure(figsize=(16,8))
     ax = plt.subplot(111)
     for matrix_idx in range(results.shape[1]):
         benefits = results[:,matrix_idx,:,1] - results[:,matrix_idx,:,0]
         plt.plot(benefits, linestyle="None",
                  marker='o', color='grey', markersize=24, alpha=0.05);
-    xlim = ax.get_xlim()    
+    xlim = ax.get_xlim()
     plt.hlines(0,*xlim, linewidth=4)
     ax.set_xlim(xlim)
-    
-    ax.set_ylabel(r"$\leftarrow$"+ "{0} better".format(methods[1]) + 
+
+    ax.set_ylabel(r"$\leftarrow$"+ "{0} better".format(methods[1]) +
                   "\t\t\t"+ "{0} better".format(methods[0]) + r"$\rightarrow$",
                   fontsize=16, fontweight="bold")
     ax.set_xlabel("Iteration Number",fontsize=16, fontweight="bold")
@@ -100,21 +126,32 @@ def plot_benefit(results, methods=["gradient_descent","newton"]):
 # functions for plotting spectral distributions
 
 def marchenkopastur_density(x, N, k, sigma=1):
+    """the density for the non-singular portion of the marchenko-pastur distribution,
+    as given by https://en.wikipedia.org/wiki/Marchenko-Pastur_distribution.
+    """
     lam = N/k
     scaling_factor = 1/(2*np.pi*sigma**2)
-    
+
     lam_plus = sigma**2*(1+np.sqrt(lam))**2
     lam_minus = sigma**2*(1-np.sqrt(lam))**2
-    
+
     if (x>lam_minus and x<lam_plus):
         return scaling_factor*(np.sqrt((lam_plus-x)*(x-lam_minus))/(lam*x))
     else:
         return 0
-    
+
 def marchenkopastur_cumulative_distribution(xs, N, k):
-    return (1-k/N)+scipy.integrate.cumtrapz([marchenkopastur_density(x, N, k) for x in xs],xs)
-                                            
+    """the cumulative distribution for the marchenko-pastur distribution,
+    calculated by numerically integrating the density for the non-singular portion
+    and then adding the singular part.
+    valid for xs>=0."""
+    return max((1-k/N), 0)+scipy.integrate.cumtrapz([marchenkopastur_density(x, N, k) for x in xs],xs)
+
 def plot_marchenko_comparison(eigvals, N, k, eps=1e-6):
+    """compare the histogram of the eigvals from a matrix
+    to the marchenko-pastur distribution with parameters N, k, and sigma=1.
+    the marchenko-pastur distribution is plotted from eps to max(eigvals) with precision 1e-5.
+    """
     plt.figure(figsize=(16,6))
 
     sns.distplot(eigvals, kde=False, bins=max(len(eigvals)//10,10),
@@ -130,12 +167,25 @@ def plot_marchenko_comparison(eigvals, N, k, eps=1e-6):
     plt.ylabel(r"$P(\Lambda \leq \lambda)$", fontsize=24)
     plt.xlabel(r"$\lambda$", fontsize=24)
     plt.legend(fontsize=16,loc=8);
-    
-# functions for testing when Newton's method fails to converge
+
+## functions for testing when Newton's method fails to converge
 
 def condition_test(kappas, hyperparameters, N=1000, k=100):
+    """applies the Newton method with provided hyperparameters for one step
+    to diagonal N by N matrices with condition numbers in kappas
+    and returns the solution_norms.
+    condition number is the ratio of largest to smallest eigenvalue.
 
-    results = []
+    matrices have a block structure, with one block of size N-k
+    equal to the identity matrix and another block of size k equal to
+    1/kappa times the identity matrix:
+
+    [[1,0, ... 0, 0],
+    [0,1, ... 0, 0],
+    [0,0, ... 1/kappa, 0],
+    [0,0, ..., 0, 1/kappa]]
+    """
+    solution_norms = []
 
     initial_values = 1/np.sqrt(N)*np.random.standard_normal(size=N).astype(np.float32)
 
@@ -144,31 +194,41 @@ def condition_test(kappas, hyperparameters, N=1000, k=100):
         matrix = np.diag(eigvals).astype(np.float32)
 
         quadratic_form = graphs.make_quadratic_form(matrix, initial_values, hyperparameters)
-        
+
         _, values = graphs.minimize(quadratic_form, "newton", 1)
 
         gradients = graphs.get_result("gradients", values, quadratic_form)[0]
 
         if np.linalg.norm(gradients)>1e-4:
             print("convergence failed on kappa={0}".format(kappa))
-            results.append(np.inf)
+            solution_norms.append(np.inf)
             continue
 
-        results.append(np.linalg.norm(values))
-    
-    return results
-    
+        solution_norms.append(np.linalg.norm(values))
+
+    return solution_norms
+
 def run_condition_tests(kappas, N, ks, hyperparameters):
+    """ runs condition_test len(ks) times with kappas, hyperparameters, and N fixed
+    and k given by ks.
+    results are returned as a list of lists of solution norms.
+    """
     results = []
-    
+
     for k in ks:
         results.append(condition_test(kappas, hyperparameters, N, k,))
-    
+
     return results
 
 def plot_results_condition_tests(kappas, results, flat_fractions, minimum_eigenvalue_magnitude):
+    """ plots the results of running run_condition_tests on kappas
+    with flat_fraction equal to k/N for each result in results.
+
+    for accurate results, should be used on outputs of run_condition_tests called with
+    the "pseudo" inverse method with minimum_eigenvalue_magnitude as its threshold.
+    """
     plt.figure(figsize=(16,6))
-    
+
     for result, flat_fraction in zip(results, flat_fractions):
         plot_result_condition_test(kappas, result, flat_fraction)
     ylims = plt.ylim()
@@ -180,12 +240,18 @@ def plot_results_condition_tests(kappas, results, flat_fractions, minimum_eigenv
     plt.xlabel(r"$\kappa$, condition number", fontsize=24)
     plt.ylim(ylims)
     plt.legend(fontsize=16);
-    
+
 def plot_result_condition_test(kappas, result, flat_fraction):
+    """ plots the results of condition_test called on kappas with parameters flat_fraction = k/N.
+    """
     plt.loglog(kappas, result, label="{0}% of directions are numerically flat".format(100*flat_fraction),
                  linestyle='None', marker='.', markersize=24, alpha=0.75);
-    
+
 def plot_condition_test_distance_scaling(distances, ks):
+    """ plots the scaling of the distances of the solutions from the true critical point
+    (which are equal to their norms, since the critical point for a quadratic form is at 0)
+    as a function of k in ks, and compares to $\sqrt(k)$ scaling.
+    """
     plt.figure(figsize=(12,6));
     plt.loglog(ks, distances, label="observed", linewidth=4);
     plt.loglog(ks, np.power(ks,0.5), label=r"$\sqrt{k}$ scaling, offset", linewidth=4);
@@ -194,14 +260,17 @@ def plot_condition_test_distance_scaling(distances, ks):
     plt.yticks(fontsize=16);
     plt.xlabel("$k$, number of flat directions", fontsize=24);
     plt.ylabel(r"$\|\|\ \theta\ \|\|$", fontsize=24);
-    
-# functions for plotting performance on neural networks
+
+## functions for plotting performance on neural networks
 
 def plot_results(gd_results, crit_finder_results, crit_finder_name):
+    """ plots gradient norms as a function of step given by Results instances
+    for g(radient) d(escent) and crit finder, the latter labeled by crit_finder_name.
+    """
     f, axs = plt.subplots(nrows=1, ncols=2, figsize=(20,6), sharey=True)
 
     axs[0].plot(gd_results.scalar_index, gd_results.gradient_norm, linewidth=4, color='C0')
-    
+
     axs[0].set_ylabel(r"$\|\|\nabla f \|\|$",fontsize=24);
     axs[0].set_xlabel("batch index", fontsize=24);
     axs[0].tick_params(axis='both', which='major', labelsize=16)
@@ -214,33 +283,52 @@ def plot_results(gd_results, crit_finder_results, crit_finder_name):
     axs[1].tick_params(axis='both', which='major', labelsize=16)
     axs[1].xaxis.set_major_formatter(matplotlib.ticker.FormatStrFormatter('%d'))
     axs[1].set_title(crit_finder_name, fontsize=28)
-    
+
 def compare_gradients_entrywise(gd_results, crit_finder_results, labels):
+    """compares the sorted entries of the first entry in gd_results.gradients (i.e. at initialization),
+    the first entry of crit_finder_results.gradients (i.e. after gradient descent has been run)
+    and the last entry of crit_finder_results (i.e. after the crit_finder has stopped running.
+    legend entries are given by labels.
+    the x axis is scaled logarithmically.
+    """
     f, ax = plt.subplots(nrows=1, ncols=1, figsize=(20,6),)
 
-    plot_gradients_entrywise(ax, gd_results.gradients[0], label=labels[0])
+    gradients = [gd_results.gradients[0], crit_finder_results.gradients[0], crit_finder_results.gradients[-1]]
 
-    plot_gradients_entrywise(ax, crit_finder_results.gradients[0], label=labels[1])
-
-    plot_gradients_entrywise(ax, crit_finder_results.gradients[-1], label=labels[2])
+    for gradient, label in zip(gradients, labels):
+        plot_gradients_entrywise(ax, gradient, label=label)
 
     plt.legend(fontsize=16)
-    
-def plot_gradients_entrywise(ax, gradients, label):
+
+def plot_gradients_entrywise(ax, gradient, label):
+    """ plots sorted entries of a single gradient vector on ax
+    with a log-scale for x and assigns legend entry label.
+    """
     ax.semilogx(range(1,len(gradients)+1), sorted(gradients), linewidth=4, label=label)
     ax.tick_params(axis='both', which='major', labelsize=16)
-    
-def compare_gradients_histogram(gd_results, crit_finder_results, titles, colors=['C0','C1','C2']):
+
+def compare_gradient_histograms(gd_results, crit_finder_results, titles, colors=['C0','C1','C2']):
+    """compares the log-scaled histogram of gradient entries for
+    the first entry in gd_results.gradients (i.e. at initialization),
+    the first entry of crit_finder_results.gradients (i.e. after gradient descent has been run)
+    and the last entry of crit_finder_results (i.e. after the crit_finder has stopped running.
+
+
+    plots are titled with titles and histograms are colored with colors.
+    """
     f, axs = plt.subplots(nrows=3, ncols=1, figsize=(20,18), sharex=True, sharey=True)
     axs[0].set_yscale('log')
-    
-    plot_gradients_histogram(axs[0], gd_results.gradients[0], title=titles[0], color = colors[0])
-    
-    plot_gradients_histogram(axs[1], crit_finder_results.gradients[0], title=titles[1], color = colors[1])
-    
-    plot_gradients_histogram(axs[2], crit_finder_results.gradients[-1], title=titles[2], color = colors[2])
-    
-def plot_gradients_histogram(ax, gradients, title, color):
-    ax.hist(gradients, normed=True, bins=100, histtype='step', color=color, linewidth=4);
+
+    gradients = [gd_results.gradients[0], crit_finder_results.gradients[0], crit_finder_results.gradients[-1]]
+
+    for gradient, ax, title, color in zip(gradients, axs, titles, colors):
+
+        plot_gradient_histogram(ax, gradient, title, color)
+
+def plot_gradient_histogram(ax, gradient, title, color):
+    """plots a histogram with hue given by color
+    of entries in gradient vector on ax and gives it title
+    """
+    ax.hist(gradient, normed=True, bins=100, histtype='step', color=color, linewidth=4);
     ax.set_title(title, fontsize=24)
     ax.tick_params(axis='both', which='major', labelsize=16)
